ac37ec6defe4036920f665494fce4a4bcca1a296
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-19 19:53:28 +0100

    Initial Commit!

---
+++ src/ci3/date_lang.php
@@ -0,0 +1,75 @@
+<?php
+
+$lang['date_year'] = 'Let';
+$lang['date_years'] = 'Leta';
+$lang['date_month'] = 'Mesec';
+$lang['date_months'] = 'Meseci';
+$lang['date_week'] = 'Teden';
+$lang['date_weeks'] = 'Tedni';
+$lang['date_day'] = 'Dan';
+$lang['date_days'] = 'Dni';
+$lang['date_hour'] = 'Ura';
+$lang['date_hours'] = 'Ure';
+$lang['date_minute'] = 'Minuta';
+$lang['date_minutes'] = 'Minute';
+$lang['date_second'] = 'Sekunda';
+$lang['date_seconds'] = 'Sekunde';
+
+$lang['date_just_now'] = 'pravkar';
+$lang['date_seconds_ago'] = '=1{pred # sekundo} =2{pred # sekundama} 3-4{pred # sekundami} other{pred # sekundami}';
+$lang['date_minutes_ago'] = '=1{pred # minuto} =2{pred # minutama} 3-4{pred # minutami} other{pred # minutami}';
+$lang['date_hours_ago'] = '=1{pred # uro} =2{pred # urama} 3-4{pred # urami} other{pred # urami}';
+$lang['date_days_ago'] = '=1{včeraj} =2{pred # dnevoma} 3-4{pred # dnevi} other{pred # dnevi}';
+$lang['date_weeks_ago'] = '=1{pred # tednom} =2{pred # tednoma} 3-4{pred # tedni} other{pred # tedni}';
+$lang['date_months_ago'] = '=1{pred # mesecem} =2{pred # mesecema} 3-4{pred # meseci} other{pred # meseci}';
+$lang['date_years_ago'] = '=1{pred # letom} =2{pred # letoma} 3-4{pred # leti} other{pred # leti}';
+
+$lang['date_su'] = 'Ne';
+$lang['date_mo'] = 'Po';
+$lang['date_tu'] = 'To';
+$lang['date_we'] = 'Sr';
+$lang['date_th'] = 'Če';
+$lang['date_fr'] = 'Pe';
+$lang['date_sa'] = 'So';
+
+$lang['date_sun'] = 'Ned';
+$lang['date_mon'] = 'Pon';
+$lang['date_tue'] = 'Tor';
+$lang['date_wed'] = 'Sre';
+$lang['date_thu'] = 'Čet';
+$lang['date_fri'] = 'Pet';
+$lang['date_sat'] = 'Sob';
+
+$lang['date_sunday'] = 'Nedelja';
+$lang['date_monday'] = 'Ponedeljek';
+$lang['date_tuesday'] = 'Torek';
+$lang['date_wednesday'] = 'Sreda';
+$lang['date_thursday'] = 'Četrtek';
+$lang['date_friday'] = 'Petek';
+$lang['date_saturday'] = 'Sobota';
+
+$lang['date_jan'] = 'Jan';
+$lang['date_feb'] = 'Feb';
+$lang['date_mar'] = 'Mar';
+$lang['date_apr'] = 'Apr';
+$lang['date_may'] = 'Maj';
+$lang['date_jun'] = 'Jun';
+$lang['date_jul'] = 'Jul';
+$lang['date_aug'] = 'Avg';
+$lang['date_sep'] = 'Sep';
+$lang['date_oct'] = 'Okt';
+$lang['date_nov'] = 'Nov';
+$lang['date_dec'] = 'Dec';
+
+$lang['date_january'] = 'Januar';
+$lang['date_february'] = 'Februar';
+$lang['date_march'] = 'Marec';
+$lang['date_april'] = 'April';
+$lang['date_mayl'] = 'Maj';
+$lang['date_june'] = 'Junij';
+$lang['date_july'] = 'Julij';
+$lang['date_august'] = 'Avgust';
+$lang['date_september'] = 'September';
+$lang['date_october'] = 'Oktober';
+$lang['date_november'] = 'November';
+$lang['date_december'] = 'December';
---
+++ src/ci3/pagination_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['pagination_first_link'] = '&lsaquo; Prva';
+$lang['pagination_last_link'] = 'Zadnja &rsaquo;';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
+$lang['pagination_summary_text'] = 'Prikazanih je %d do %d od %d vnosov';
